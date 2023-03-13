ea6ce1f86b521ed6672367b1631eabe00afb1456
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 15:26:30 +0700

    fix(PhoneNumber component): adjust width

--- src/lib/components/PhoneNumber.svelte
+++ src/lib/components/PhoneNumber.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <Select
-	class="w-2/6 md:w-2/6"
+	class="w-3/6"
 	items={normalizedCountries.map((country) => ({
 		value: country.iso2,
 		name: `${countryCodeToFlagEmoji(country.iso2)}
@@ -45,6 +45,6 @@
 	bind:country={selectedCountry}
 	bind:value
 	bind:valid
-	class="block p-2.5 w-4/6 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
+	class="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
 		'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
 />
