a3a0a50bd7ab8f6e15c6ca6226839ba7a1f7b76f
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 13:52:03 +0700

    fix(layout): adjust padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 	import '../app.css';
 </script>
 
-<main class="container p-4 mx-auto lg:py-8 lg:px-4">
+<main class="container p-4 mx-auto md:py-4 md:px-8 lg:py-8 lg:px-16 xl:py-16 xl:px-32">
 	<slot />
 </main>
