37c12ab6b0475883e36295a560d4fb5014af31c0
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 07:11:03 +0700

    chore: setup tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
