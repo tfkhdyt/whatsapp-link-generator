ed1c8463184f80941c10fbf9e15d9b1738fd0762
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 23:30:42 +0700

    refactor(page): refactor some action to functions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import { fade, slide } from 'svelte/transition';
 	import PhoneNumber from '../lib/components/PhoneNumber.svelte';
 	import { idIncrement, phoneNumbers } from '../stores/phoneNumber';
+	import type { PhoneNumberType } from '../types/PhoneNumber';
 
 	let message = '';
 	$: numOfPhone = $phoneNumbers.length > 1 ? $phoneNumbers.length : 1;
@@ -17,6 +18,30 @@
 
 	function removeField(id: number) {
 		$phoneNumbers = $phoneNumbers.filter((num) => num.id !== id);
+	}
+
+	function formatLink(number: PhoneNumberType, message: string) {
+		return `https://wa.me/${number.value.replace('+', '')}${
+			message.trim() ? `?text=${encodeURI(message.trim())}` : ''
+		}`;
+	}
+
+	function copyLink(number: PhoneNumberType, message: string) {
+		navigator.clipboard.writeText(formatLink(number, message));
+	}
+
+	function copyAll() {
+		const phones = $phoneNumbers
+			.filter((num) => num.valid && num.value !== '')
+			.map(
+				(num) =>
+					`https://wa.me/${num.value.replace('+', '')}${
+						message.trim() ? `?text=${encodeURI(message.trim())}` : ''
+					}`
+			)
+			.join('\n');
+
+		navigator.clipboard.writeText(phones);
 	}
 </script>
 
@@ -32,7 +57,7 @@
 <Heading tag="h3" class="mb-6">WhatsApp Link Generator</Heading>
 <Label defaultClass="mb-2 font-bold block">WhatsApp Numbers:</Label>
 {#each $phoneNumbers as _, index ($phoneNumbers[index].id)}
-	<div class="flex mr-2 mb-2 space-x-2 w-full lg:w-3/6" transition:slide|local>
+	<div class="flex mr-2 mb-2 space-x-2 w-full" transition:slide|local>
 		<PhoneNumber bind:value={$phoneNumbers[index].value} bind:valid={$phoneNumbers[index].valid} />
 
 		{#if numOfPhone > 1}
@@ -77,22 +102,16 @@
 	name="message"
 	bind:value={message}
 	placeholder="Assalamu'alaikum Wr. Wb."
-	class="w-full lg:w-3/6"
+	class="w-full"
 />
 
 {#if showResult}
-	<P class="mt-6 mb-2" weight="bold">Result:</P>
+	<P class="mt-6" weight="bold">Result:</P>
 	<List tag="ol" list="decimal">
 		{#each $phoneNumbers.filter((num) => num.valid && num.value !== '') as number (number.id)}
 			<span transition:fade animate:flip>
 				<Li class="mb-2">
-					<A
-						href={`https://wa.me/${number.value.replace('+', '')}${
-							message.trim() ? `?text=${encodeURI(message.trim())}` : ''
-						}`}
-						target="_blank"
-						class="mr-2 font-medium"
-					>
+					<A href={formatLink(number, message)} target="_blank" class="mr-2 font-medium">
 						wa.me/{number.value.replace('+', '')}
 					</A>
 					<Button
@@ -100,12 +119,7 @@
 						outline
 						pill={true}
 						size="xs"
-						on:click={async () =>
-							await navigator.clipboard.writeText(
-								`https://wa.me/${number.value.replace('+', '')}${
-									message.trim() ? `?text=${encodeURI(message.trim())}` : ''
-								}`
-							)}
+						on:click={() => copyLink(number, message)}
 					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -128,6 +142,25 @@
 			</span>
 		{/each}
 	</List>
+
+	<Button id="copyAll" outline pill={true} size="xs" on:click={copyAll}>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="mr-1 w-4 h-4"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+			/>
+		</svg>
+		Copy All</Button
+	>
+	<Tooltip trigger="click" triggeredBy="#copyAll">Copied</Tooltip>
 {/if}
 
 <P align="center" weight="light" class="mt-6">
