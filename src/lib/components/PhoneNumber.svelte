0cf97210a136666b2c7a70e75f17f865cce1fa91
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 13:52:17 +0700

    fix(phone number): adjust padding

--- src/lib/components/PhoneNumber.svelte
+++ src/lib/components/PhoneNumber.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <Select
-	class="w-5/12 md:w-2/6"
+	class="w-2/6 md:w-2/6"
 	items={normalizedCountries.map((country) => ({
 		value: country.iso2,
 		name: `${countryCodeToFlagEmoji(country.iso2)}
@@ -25,6 +25,6 @@
 	bind:country={selectedCountry}
 	bind:value
 	bind:valid
-	class="block p-2.5 w-7/12 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
+	class="block p-2.5 w-4/6 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
 		'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
 />
