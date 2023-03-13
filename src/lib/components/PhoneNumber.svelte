2f77a01c16f1fe61a80f37ca8d54dfd8c56c5ccf
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 10:56:26 +0700

    feat(PhoneNumber component): show country flag

--- src/lib/components/PhoneNumber.svelte
+++ src/lib/components/PhoneNumber.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
 	import TelInput, { normalizedCountries } from 'svelte-tel-input';
-	import type { NormalizedTelNumber, CountryCode, E164Number } from 'svelte-tel-input/types';
-
-	// Any Country Code Alpha-2 (ISO 3166)
-	let country: CountryCode | null = 'ID';
+	import type { CountryCode, E164Number } from 'svelte-tel-input/types';
+	import countryCodeToFlagEmoji from 'country-code-to-flag-emoji';
 
 	// You must use E164 number format. It's guarantee the parsing and storing consistency.
-	export let value: E164Number | null = '';
+	export let value: E164Number = '';
 
-	// Validity
-	let valid = true;
+	export let valid = true;
 
-	// Optional - Extended details about the parsed phone number
-	let parsedTelInput: NormalizedTelNumber | null = null;
-
-	let selectedCountry = 'ID';
+	let selectedCountry: CountryCode = 'ID';
 </script>
 
 <div class="flex mr-2 mb-2 space-x-2">
 	<select
-		class="block p-2.5 w-2/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500"
+		class="block p-2.5 w-5/12 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 md:w-2/6 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500"
 		aria-label="Default select example"
 		name="Country"
 		bind:value={selectedCountry}
@@ -31,15 +25,16 @@
 				selected={country.iso2 === selectedCountry}
 				aria-selected={country.iso2 === selectedCountry}
 			>
-				{country.iso2} (+{country.dialCode})
+				{countryCodeToFlagEmoji(country.iso2)}
+				(+{country.dialCode}) {country.name.split(' (')[0]}
 			</option>
 		{/each}
 	</select>
 	<TelInput
-		bind:country
+		bind:country={selectedCountry}
 		bind:value
 		bind:valid
-		bind:parsedTelInput
-		class="block p-2.5 w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500"
+		class="block p-2.5 w-7/12 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
+			'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
 	/>
 </div>
