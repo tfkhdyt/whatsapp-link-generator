433a02a563e3dfbd1cee3b9f31d809522dfb5ea4
tfkhdyt <tfkhdyt@proton.me>
2023-03-14 06:45:41 +0700

    refactor(button): move AddFieldButton to separate file

---
+++ src/lib/components/Buttons/AddFieldButton.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Button } from 'flowbite-svelte';
+
+	import { addField } from '$lib/fields';
+</script>
+
+<Button color="alternative" class="mb-6" on:click={addField}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="mr-2 w-5 h-5"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+	</svg>
+	Add number
+</Button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 	import SvelteSeo from 'svelte-seo';
 
 	import { copyAll, copyLink } from '$lib/clipboard';
+	import AddFieldButton from '$lib/components/Buttons/AddFieldButton.svelte';
 	import PhoneNumber from '$lib/components/PhoneNumber.svelte';
-	import { addField } from '$lib/fields';
 	import { formatLink } from '$lib/format';
 	import { phoneNumbers } from '../stores/phoneNumber';
 
@@ -35,19 +35,7 @@
 	/>
 {/each}
 
-<Button color="alternative" class="mb-6" on:click={addField}>
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke-width="1.5"
-		stroke="currentColor"
-		class="mr-2 w-5 h-5"
-	>
-		<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-	</svg>
-	Add number
-</Button>
+<AddFieldButton />
 
 <Label for="message" defaultClass="mb-2 font-bold block">Message:</Label>
 <Textarea
