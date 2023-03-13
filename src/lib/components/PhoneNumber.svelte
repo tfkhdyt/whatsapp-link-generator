705b0d2950ab76072f5a4308fcfd5a9b8f1a1797
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 12:54:46 +0700

    perf: switch to flowbite-svelte components

--- src/lib/components/PhoneNumber.svelte
+++ src/lib/components/PhoneNumber.svelte
@@ -2,6 +2,7 @@
 	import TelInput, { normalizedCountries } from 'svelte-tel-input';
 	import type { CountryCode, E164Number } from 'svelte-tel-input/types';
 	import countryCodeToFlagEmoji from 'country-code-to-flag-emoji';
+	import { Select } from 'flowbite-svelte';
 
 	// You must use E164 number format. It's guarantee the parsing and storing consistency.
 	export let value: E164Number = '';
@@ -11,30 +12,19 @@
 	let selectedCountry: CountryCode = 'ID';
 </script>
 
-<div class="flex mr-2 mb-2 space-x-2 w-full lg:w-3/6">
-	<select
-		class="block p-2.5 w-5/12 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 md:w-2/6 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500"
-		aria-label="Default select example"
-		name="Country"
-		bind:value={selectedCountry}
-	>
-		<option value={null} hidden={selectedCountry !== null}>Please select</option>
-		{#each normalizedCountries as country (country.id)}
-			<option
-				value={country.iso2}
-				selected={country.iso2 === selectedCountry}
-				aria-selected={country.iso2 === selectedCountry}
-			>
-				{countryCodeToFlagEmoji(country.iso2)}
-				(+{country.dialCode}) {country.name.split(' (')[0]}
-			</option>
-		{/each}
-	</select>
-	<TelInput
-		bind:country={selectedCountry}
-		bind:value
-		bind:valid
-		class="block p-2.5 w-7/12 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
-			'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
-	/>
-</div>
+<Select
+	class="w-5/12 md:w-2/6"
+	items={normalizedCountries.map((country) => ({
+		value: country.iso2,
+		name: `${countryCodeToFlagEmoji(country.iso2)}
+				(+${country.dialCode}) ${country.name.split(' (')[0]}`
+	}))}
+	bind:value={selectedCountry}
+/>
+<TelInput
+	bind:country={selectedCountry}
+	bind:value
+	bind:valid
+	class="block p-2.5 w-7/12 md:w-4/6 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
+		'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
+/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Tooltip, Button } from 'flowbite-svelte';
+	import { Tooltip, Button, Heading, P, Label, Textarea, List, Li, A } from 'flowbite-svelte';
 	import PhoneNumber from '../lib/components/PhoneNumber.svelte';
 	import { idIncrement, phoneNumbers } from '../stores/phoneNumber';
 
@@ -18,28 +18,24 @@
 </script>
 
 <main>
-	<h1 class="mb-6 text-3xl font-bold">WhatsApp Link Generator</h1>
-	<p class="mb-2 font-bold">WhatsApp Numbers</p>
+	<Heading tag="h3" class="mb-6">WhatsApp Link Generator</Heading>
+	<Label defaultClass="mb-2 font-bold block">WhatsApp Numbers</Label>
 	{#each $phoneNumbers as _, index ($phoneNumbers[index].id)}
-		<div class="flex">
+		<div class="flex mr-2 mb-2 space-x-2 w-full lg:w-3/6">
 			<PhoneNumber
 				bind:value={$phoneNumbers[index].value}
 				bind:valid={$phoneNumbers[index].valid}
 			/>
 
 			{#if numOfPhone > 1}
-				<button
-					type="button"
-					class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600 hover:text-blue-700 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 focus:outline-none dark:focus:ring-gray-700 dark:hover:text-white dark:hover:bg-gray-700"
-					on:click={() => removeField($phoneNumbers[index].id)}
-				>
+				<Button color="alternative" on:click={() => removeField($phoneNumbers[index].id)}>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						viewBox="0 0 24 24"
 						stroke-width="1.5"
 						stroke="currentColor"
-						class="w-6 h-6"
+						class="w-5 h-5"
 					>
 						<path
 							stroke-linecap="round"
@@ -47,16 +43,12 @@
 							d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
 						/>
 					</svg>
-				</button>
+				</Button>
 			{/if}
 		</div>
 	{/each}
 
-	<button
-		type="button"
-		class="flex py-2.5 px-5 mr-2 mb-6 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600 hover:text-blue-700 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 focus:outline-none dark:focus:ring-gray-700 dark:hover:text-white dark:hover:bg-gray-700"
-		on:click={addField}
-	>
+	<Button color="alternative" class="mb-6" on:click={addField}>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
@@ -68,31 +60,30 @@
 			<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 		</svg>
 		Add number
-	</button>
+	</Button>
 
-	<label for="message" class="block mb-2 font-bold text-gray-900 dark:text-white text-md"
-		>Message</label
-	>
-	<textarea
+	<Label for="message" defaultClass="mb-2 font-bold block">Message</Label>
+	<Textarea
 		id="message"
 		rows="4"
-		class="block p-2.5 mb-6 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 lg:w-3/6 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-md dark:focus:ring-blue-500 dark:focus:border-blue-500"
-		placeholder="Assalamu'alaikum Wr. Wb."
+		name="message"
 		bind:value={message}
+		placeholder="Assalamu'alaikum Wr. Wb."
+		class="w-full lg:w-3/6"
 	/>
 
 	{#if showResult}
-		<p class="mb-2 font-bold">Result</p>
-		<ul class="space-y-1 max-w-md list-disc list-inside text-gray-500 dark:text-gray-400">
+		<P class="mt-6 mb-2" weight="bold">Result</P>
+		<List tag="ul">
 			{#each $phoneNumbers.filter((num) => num.valid && num.value !== '') as number}
-				<li>
-					<a
+				<Li>
+					<A
 						href="https://wa.me/{number.value.replace('+', '')}?text={encodeURI(message)}"
 						target="_blank"
-						class="mr-2 font-medium text-blue-600 dark:text-blue-500 hover:underline"
+						class="mr-2 font-medium"
 					>
 						wa.me/{number.value.replace('+', '')}
-					</a>
+					</A>
 					<Button
 						id="click{number.id}"
 						outline
@@ -120,8 +111,8 @@
 						Copy link</Button
 					>
 					<Tooltip trigger="click" triggeredBy="#click{number.id}">Copied</Tooltip>
-				</li>
+				</Li>
 			{/each}
-		</ul>
+		</List>
 	{/if}
 </main>
