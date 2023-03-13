63460a91da0c4acba3352feb85c5717ed6fc52c6
tfkhdyt <tfkhdyt@proton.me>
2023-03-13 15:51:28 +0700

    fix(PhoneNumber component): revert to plain element

--- src/lib/components/PhoneNumber.svelte
+++ src/lib/components/PhoneNumber.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import TelInput, { normalizedCountries } from 'svelte-tel-input';
 	import type { CountryCode, E164Number } from 'svelte-tel-input/types';
-	import { Select } from 'flowbite-svelte';
 
 	// You must use E164 number format. It's guarantee the parsing and storing consistency.
 	export let value: E164Number = '';
@@ -32,19 +31,28 @@
 	}
 </script>
 
-<Select
-	defaultClass="w-3/6 text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-	items={normalizedCountries.map((country) => ({
-		value: country.iso2,
-		name: `${countryCodeToFlagEmoji(country.iso2)}
-				(+${country.dialCode}) ${country.name.split(' (')[0]}`
-	}))}
+<select
+	class="block p-2.5 w-3/6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+	name="Country"
 	bind:value={selectedCountry}
-/>
+>
+	<option value={null} hidden={selectedCountry !== null}>Please select</option>
+	{#each normalizedCountries as country (country.id)}
+		<option
+			value={country.iso2}
+			selected={country.iso2 === selectedCountry}
+			aria-selected={country.iso2 === selectedCountry}
+		>
+			{countryCodeToFlagEmoji(country.iso2)}
+			(+{country.dialCode}) {country.name}
+		</option>
+	{/each}
+</select>
+
 <TelInput
 	bind:country={selectedCountry}
 	bind:value
 	bind:valid
 	class="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 dark:placeholder-gray-400 dark:text-white dark:bg-gray-700 dark:border-gray-600 focus:border-blue-500 focus:ring-blue-500 text-sm dark:focus:ring-blue-500 dark:focus:border-blue-500 {!valid &&
-		'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
+		'bg-red-50 border-red-500 text-red-900 focus:ring-red-500 focus:border-red-500 dark:focus:ring-red-500 dark:focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'}"
 />
