512317fbe253a9408e0a4106abb50a42e42d4d5b
tfkhdyt <tfkhdyt@proton.me>
2023-03-14 06:36:22 +0700

    refactor(PhoneNumber): move remove button to the inside of PhoneNumber component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 	import { copyAll, copyLink } from '$lib/clipboard';
 	import PhoneNumber from '$lib/components/PhoneNumber.svelte';
-	import { addField, removeField } from '$lib/fields';
+	import { addField } from '$lib/fields';
 	import { formatLink } from '$lib/format';
 	import { phoneNumbers } from '../stores/phoneNumber';
 
@@ -28,26 +28,12 @@
 <Label defaultClass="mb-2 font-bold block">WhatsApp Numbers:</Label>
 {#each $phoneNumbers as _, index ($phoneNumbers[index].id)}
 	<div class="flex mr-2 mb-2 space-x-2 w-full" transition:slide|local>
-		<PhoneNumber bind:value={$phoneNumbers[index].value} bind:valid={$phoneNumbers[index].valid} />
-
-		{#if numOfPhone > 1}
-			<Button color="alternative" on:click={() => removeField($phoneNumbers[index].id)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-5 h-5"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-					/>
-				</svg>
-			</Button>
-		{/if}
+		<PhoneNumber
+			bind:value={$phoneNumbers[index].value}
+			bind:valid={$phoneNumbers[index].valid}
+			{numOfPhone}
+			id={$phoneNumbers[index].id}
+		/>
 	</div>
 {/each}
 
