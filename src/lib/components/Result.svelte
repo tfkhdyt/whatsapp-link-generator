392e60ae676124d6a2ce72d94a6c42e98c30d0b8
tfkhdyt <tfkhdyt@proton.me>
2023-03-14 07:00:40 +0700

    refactor(result): move copy link button into separate component

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -3,7 +3,8 @@
 	import { flip } from 'svelte/animate';
 	import { fade } from 'svelte/transition';
 
-	import { copyAll, copyLink } from '$lib/clipboard';
+	import CopyLinkButton from './Buttons/CopyLinkButton.svelte';
+	import { copyAll } from '$lib/clipboard';
 	import { formatLink } from '$lib/format';
 	import { phoneNumbers } from '../../stores/phoneNumber';
 
@@ -18,30 +19,7 @@
 				<A href={formatLink(number, message)} target="_blank" class="mr-2 font-medium">
 					wa.me/{number.value.replace('+', '')}
 				</A>
-				<Button
-					id="click{number.id}"
-					outline
-					pill={true}
-					size="xs"
-					on:click={() => copyLink(number, message)}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="mr-1 w-4 h-4"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-						/>
-					</svg>
-					Copy link</Button
-				>
-				<Tooltip trigger="click" triggeredBy="#click{number.id}">Copied</Tooltip>
+				<CopyLinkButton id="click{number.id}" {number} {message} />
 			</Li>
 		</span>
 	{/each}
